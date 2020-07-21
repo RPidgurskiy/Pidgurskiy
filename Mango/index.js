b47f947a8f2c13bc389ccbc4b345d9a104c6ca4c
Pidgurskiy Ruslan <45541046+RPidgurskiy@users.noreply.github.com>
2020-07-22 02:20:36 +0300

    Add files via upload

--- Mango/index.js
+++ Mango/index.js
@@ -135,7 +135,33 @@ function scrollNav() {
 
   } else {
     document.getElementById("navbar").style.backgroundColor = "";
-    document.getElementById("logo").style.width = "120px";
+    document.getElementById("logo").style.width = "100px";
 
+  }
+}
+
+
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
